ec8fea19799dbc8dcdc62c9646b3b512a8f04bfe
doughnutnz <dplhunt@gmail.com>
2018-12-05 18:03:26 +1300

    Set up Python virtual environment (venv) and tidy code

--- testget.py
+++ testget.py
@@ -1,17 +1,23 @@
-# import requests library
+# # import requests library
 import requests
+
+KEY_FILE = ".Penviron" # Define file name for subscription key
+fin = open(KEY_FILE, "r")  # Open key file, assumed it is there
+
+keydat = fin.read();  # Read the key from the key file, assuming correct format and only data
+# print("read data is : ", keydat)
 
 # api-endpoint
 # Auckland transport
 URL = "https://api.at.govt.nz/v2/gtfs/agency"  # AT url
 # AT headers
-HEADERS = {'Ocp-Apim-Subscription-Key': "a0e149fb205d41b5bbfda64798c9d55b"}
+HEADERS = {'Ocp-Apim-Subscription-Key': keydat}
 PARAMS = {'callback': 'JSON'}  # AT params
 
 # Currently Google Maps
 # URL = "http://maps.googleapis.com/maps/api/geocode/json"  # G Maps
 
-# Maps loaction specified here
+# Maps location specified here
 # location = "AUT University, Auckland"  # G Maps
 
 # Define a dictionary with the needed params for G Maps
@@ -28,7 +34,11 @@ print(sendurl)
 print(r.status_code)
 print(type(data))
 print(data)
-print(data[status])
+print(data["status"])
+print(type(data["response"]))
+print(data["response"][0])
+print(data["response"][0]["agency_name"])
+print(data["error"])
 # Now extract latitude, longitude and formatted address
 #   of the first matching location
 # latitude = data['results'][0]['geometry']['location']['lat']
